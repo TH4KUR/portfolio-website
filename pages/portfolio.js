5b3eab09a7f8ac226344a94b51a3a10339ab7429
Eashaan Thakur <81467048+TH4KUR@users.noreply.github.com>
2022-04-03 21:49:38 +0530

    fix modal hiding behind navbar

---
+++ pages/portfolio.js
@@ -0,0 +1,10 @@
+export default function portfolio() {
+    return (
+      <>
+      <div className='h-[100vh] w-full b-dark-4 text-gray-100 grid place-items-center text-2xl'>
+        Resume.pdf is not yet available
+      </div>
+      </>
+    );
+  }
+  
